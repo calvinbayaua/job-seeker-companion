9c306c192e87089bd12025cb4dbd4aa58a5adb01
calvinbayaua <38298812+calvinbayaua@users.noreply.github.com>
2025-06-04 16:32:57 -0700

    -Updated database schema to reflect new form inputs
    -POST requests to /api/applicatons/submit now successfully saves new applications to MongoDB

--- server/models/application.js
+++ server/models/application.js
@@ -3,8 +3,14 @@ const mongoose = require("mongoose");
 const applicationSchema = new mongoose.Schema({
   companyName: String,
   jobTitle: String,
+  jobType: String,
   location: String,
+  locationType: String,
   date: Date,
+  link: String,
+  easyApply: String,
+  coverLetterSent: String,
+  status: String,
 });
 
 module.exports = mongoose.model("Application", applicationSchema);
--- server/routes/applications.js
+++ server/routes/applications.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const Application = require("../models/application.js");
 
-router.post("/submit", (req, res) => {
-  console.log(req.body);
-  res.send("Success!");
+router.post("/submit", async (req, res) => {
+  try {
+    const newApplication = new Application(req.body);
+    await newApplication.save();
+    return res.status(200).json({ message: "Applicaton added successfully!" });
+  } catch (error) {
+    console.error("Error adding new application to database:", error);
+    return res.status(500).json({ message: "Failed to add new applicaton." });
+  }
 });
 
 module.exports = router;
